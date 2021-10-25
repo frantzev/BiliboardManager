a8189ed56dc3716f41742e605c06a4482f2adfcd
Францев Алексей Евгеньевич <frantsev_ae@kms.tvz>
2021-10-26 02:08:53 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Film.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Film {
+    private int id;
+    private String name;
+    private String genre;
+    private String Url;
+}
